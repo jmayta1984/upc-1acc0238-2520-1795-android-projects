8ddcb4175794ee6926adab02a74dfc8fcaa585c7
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-12 17:14:45 -0500

    feat: add categories & banner

--- EasyShop/app/src/main/java/pe/edu/upc/easyshop/ui/Home.kt
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/ui/Home.kt
@@ -1,16 +1,22 @@
 package pe.edu.upc.easyshop.ui
 
+import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
@@ -18,23 +24,37 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.FilterList
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import pe.edu.upc.easyshop.R
 import pe.edu.upc.easyshop.ui.theme.EasyShopTheme
 
 @Composable
 fun Home() {
+
+    val categories = listOf("All", "Men", "Women", "Boys", "Girls")
+
+    val selectedCategory = remember {
+        mutableStateOf("All")
+    }
     Column(modifier = Modifier.fillMaxSize()) {
         Row(
             modifier = Modifier
@@ -111,6 +131,77 @@ fun Home() {
             )
             TextButton(onClick = {}) { Text("See all") }
         }
+
+        LazyRow {
+            items(categories) { category ->
+                FilterChip(
+                    selected = selectedCategory.value == category,
+                    onClick = {
+                        selectedCategory.value = category
+                    },
+                    label = { Text(category) },
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                )
+            }
+        }
+
+        Box(modifier = Modifier.padding(8.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(192.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(
+                        brush = Brush.linearGradient(
+                            listOf(
+                                MaterialTheme.colorScheme.primary,
+                                MaterialTheme.colorScheme.inversePrimary
+                            )
+                        )
+                    )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxHeight()
+                        .weight(1f),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        "Get your special sale up to 40%",
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        style = MaterialTheme.typography.headlineSmall,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    ElevatedButton(onClick = {}) { Text("Shop now") }
+                }
+                Image(
+                    painterResource(R.drawable.banner),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+
+                    )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Text(
+                "Products",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.weight(1f)
+            )
+            TextButton(onClick = {}) { Text("See all") }
+        }
     }
 }
 
@@ -118,6 +209,7 @@ fun Home() {
 fun WidthSpacer() {
     Spacer(modifier = Modifier.width(8.dp))
 }
+
 
 @Composable
 fun RoundedIcon(icon: ImageVector) {
@@ -134,6 +226,7 @@ fun RoundedIcon(icon: ImageVector) {
                 .fillMaxSize()
                 .padding(8.dp)
         )
+
     }
 }
 
