5aa75280b28cad502a43c024ac7bc3d59b2d0bd6
jmayta1984 <jmayta@pucp.edu.pe>
2025-10-01 07:24:47 -0500

    chore: update project

--- EasyShop/app/src/main/java/pe/edu/upc/easyshop/core/navigation/AppNavigation.kt
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/core/navigation/AppNavigation.kt
@@ -3,24 +3,26 @@ package pe.edu.upc.easyshop.core.navigation
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import pe.edu.upc.easyshop.core.root.Main
 import pe.edu.upc.easyshop.core.ui.theme.EasyShopTheme
 import pe.edu.upc.easyshop.features.auth.presentation.di.PresentationModule.getLoginViewModel
 import pe.edu.upc.easyshop.features.auth.presentation.login.Login
 import pe.edu.upc.easyshop.features.home.presentation.di.PresentationModule.getProductDetailViewModel
+import pe.edu.upc.easyshop.features.home.presentation.productdetail.ProductDetail
 
 @Composable
-fun AppNavigation(){
+fun AppNavigation() {
     val navController = rememberNavController()
     val loginViewModel = getLoginViewModel()
-    val productDetailViewModel = getProductDetailViewModel()
 
 
     NavHost(navController, startDestination = Route.Login.route) {
-        composable(Route.Login.route){
+        composable(Route.Login.route) {
             Log.d("AppNavigation", "Login")
             Login(loginViewModel) {
                 navController.navigate(Route.Main.route)
@@ -30,7 +32,7 @@ fun AppNavigation(){
         }
 
 
-        composable (Route.Main.route){
+        composable(Route.Main.route) {
             Log.d("AppNavigation", "Main")
 
             Main { productId ->
@@ -38,8 +40,8 @@ fun AppNavigation(){
 
             }
         }
-/*
-        composable (
+
+        composable(
             route = Route.ProductDetail.routeWithArgument,
             arguments = listOf(navArgument(Route.ProductDetail.argument) {
                 type = NavType.IntType
@@ -49,24 +51,24 @@ fun AppNavigation(){
 
                 val productId = arguments.getInt(Route.ProductDetail.argument)
                 Log.d("AppNavigation", productId.toString())
+                val productDetailViewModel = getProductDetailViewModel()
 
-                //productDetailViewModel.getProductById(productId)
-                //ProductDetail(productDetailViewModel)
+                productDetailViewModel.getProductById(productId)
+                ProductDetail(productDetailViewModel)
 
 
             }
 
         }
 
- */
+
     }
 }
 
 
-
 @Preview(showBackground = true)
 @Composable
-fun AppNavigationPreview(){
+fun AppNavigationPreview() {
     EasyShopTheme {
         AppNavigation()
     }
--- EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/productdetail/ProductDetail.kt
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/productdetail/ProductDetail.kt
@@ -1,13 +1,33 @@
 package pe.edu.upc.easyshop.features.home.presentation.productdetail
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import pe.edu.upc.easyshop.core.ui.components.RoundedIcon
 
 @Composable
 fun ProductDetail(viewModel: ProductDetailViewModel) {
     Log.d("ProductDetail", viewModel.toString())
 
-    /*
+
 
       val product = viewModel.product.collectAsState()
 
@@ -58,5 +78,5 @@ fun ProductDetail(viewModel: ProductDetailViewModel) {
       }
 
 
-     */
+
 }
