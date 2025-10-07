58b13ac0030a2e28add14ef5fe4b8fa11a987250
jmayta1984 <jmayta@pucp.edu.pe>
2025-10-06 21:18:41 -0500

    chore: update project

--- EasyMovie/app/src/main/java/pe/edu/upc/easymovie/features/movies/presentation/MovieCard.kt
+++ EasyMovie/app/src/main/java/pe/edu/upc/easymovie/features/movies/presentation/MovieCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -16,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -39,7 +41,8 @@ fun MovieCard(
                 contentDescription = null,
                 modifier = Modifier
                     .height(92.dp)
-                    .width(56.dp),
+                    .width(56.dp)
+                    .clip(RoundedCornerShape(8.dp)),
                 contentScale = ContentScale.FillHeight
             )
 
